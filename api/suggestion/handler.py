1f68fe3aeff72c05c61f13f2e0400dba2bcf8e4c
Henry Goddard <godd9170@mylaurier.ca>
2016-07-18 13:29:30 -0400

    birth of the suggestion endpoint :tada:

--- api/suggestion/handler.py
+++ api/suggestion/handler.py
@@ -7,5 +7,14 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 def handler(event, context):
-    log.debug("Received event {}".format(json.dumps(event)))
-    return {}
+	log.debug("Received event {}".format(json.dumps(event)))
+	response = {}
+	if event.get('path') == '/suggestion' and event.get('method') == 'POST':
+		if event.get('body') and event.get('body').get('id'):
+			body = event.get('body')
+			response = get_account(body.get('id'))[0]
+		else:
+			response['Error'] = "No Body"
+	else:
+		response['Error'] = "No such endpoint"
+	return response
