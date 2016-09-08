b3486fcd926fc1ed0179a7e12cda3e98cf461bab
Henry Goddard <godd9170@mylaurier.ca>
2016-09-08 14:54:19 -0400

    woohoo! utils are callable as microservices from :tada:

--- api/account/handler.py
+++ api/account/handler.py
@@ -1,4 +1,12 @@
-from utils.test import hey
-
+# Thank you http://stackoverflow.com/questions/36784925/how-to-get-return-response-from-aws-lambda-function
+import boto3
+import json
 def account(event, context):
-	return { "message from lib": hey("Henry") }
+	client = boto3.client('lambda')
+	response = client.invoke(
+		FunctionName='salesforce-dev-get',
+		InvocationType='RequestResponse'
+		#Payload=b'bytes'|file,
+	)
+	message = json.loads(response['Payload'].read()).get('message')
+	return { "message from lib": message }
