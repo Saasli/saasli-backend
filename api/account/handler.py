d587e275af7a418e34c4c596847001ab621ee532
Henry Goddard <godd9170@mylaurier.ca>
2016-09-09 17:10:37 -0400

    securely store and fetch user credentials closes #5

--- api/account/handler.py
+++ api/account/handler.py
@@ -1,10 +1,12 @@
-from tools import Microservice
+from tools import Microservice, Credentials
+import json
 functions = Microservice() # a global instantiation of the boto lambda abstraction
 
 def account(event, context):
-	payload = {'parameter' : 'value'}
-
-	response = functions.request('salesforce-dev-get', payload)
-	message = response.get('message')
-	back = response.get('back')
-	return { "message from lib": message, "paramter passed through" : back }
+	try:
+		# Get the Salesforce Credentials
+		credentials = Credentials(event.get('body').get('id'))
+		response = credentials.__dict__
+	except:
+		response = {'Error' : 'Invalid Parameters'}
+	return response
--- api/account/tools.py
+++ api/account/tools.py
@@ -11,7 +11,36 @@ class Microservice(object):
 	# returns the response of the lambda function
 	def request(self, function, payload):
 		return json.loads( self.client.invoke(
-		FunctionName=function,
-		InvocationType='RequestResponse',
-		Payload=json.dumps(payload).encode()
+			FunctionName=function,
+			InvocationType='RequestResponse',
+			Payload=json.dumps(payload).encode()
 		)['Payload'].read())
+
+
+#This class expects that stored in the 'credentials' attribute of the client 
+#is an encrypted base64 stringified piece of json.
+class Credentials(object):
+	def __init__(self, id):
+		try:
+			print "CREDENTIALS: %s" % id
+			functions = Microservice()
+			# Get the encrypted data for the client
+			dynamoPayload = {
+				'id' : id,
+				'key' : 'id',
+				'tablename' : 'clients'
+			}
+			encryptedCredentials = functions.request('dynamodb-dev-getitem', dynamoPayload)
+			print "ENCRYPTED: %s" % encryptedCredentials
+			# Decrypt the client credentials
+			kmsPayload = { 
+				'cipher' : encryptedCredentials.get('credentials').get('S')
+			}
+			print "KMS PAYLOAD: %s" % kmsPayload
+			for key, value in json.loads(functions.request('kms-dev-decrypt', kmsPayload)).iteritems():
+				print ("KEY: %s VALUE: %s" % (key,value))
+				setattr(self, key, value)
+			print "DONE CREDS"
+
+		except:
+			print 'No Such Client'
