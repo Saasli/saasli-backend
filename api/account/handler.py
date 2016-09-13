6a4f6b233ce6cb971838e00b884f10936cdfffac
Henry Goddard <godd9170@mylaurier.ca>
2016-09-12 20:29:43 -0400

    functioning account endpoint compliant with docs :tada: closes #7

--- api/account/handler.py
+++ api/account/handler.py
@@ -1,17 +1,20 @@
 from tools import Microservice, Credentials
 functions = Microservice() # a global instantiation of the boto lambda abstraction
 
+
+#Expecting a payload as defined here: https://saasli.github.io/docs/#account
 def account(event, context):
+	body = event.get('body')
 	try:
 		# Get the Salesforce Credentials & add to query payload
-		credentials = Credentials(event.get('body').get('id'))
-		query_payload = credentials.__dict__
+		credentials = Credentials(body.get('client_id'))
+		account_payload = credentials.__dict__
 		# Get the first record
-		query_payload['sf_object_id'] = 'Contact'
-		query_payload['sf_field_id'] = 'Email'
-		query_payload['sf_field_value'] = 'hgoddard@saasli.com'
-		query_payload['sf_select_fields'] = ['Id', 'Name']
-		response = functions.request('salesforce-rest-dev-get', query_payload)
+		account_payload['sf_object_id'] = 'Account' #hardcoded by virtue of endpoint being Account
+		account_payload['sf_field_id'] = body.get('sf_field_id')
+		account_payload['sf_field_value'] = body.get('sf_field_value')
+		account_payload['sf_values'] = body.get('sf_values')
+		response = functions.request('salesforce-rest-dev-put', account_payload)
 		print response
 	except:
 		response = {'Error' : 'Invalid Parameters'}
