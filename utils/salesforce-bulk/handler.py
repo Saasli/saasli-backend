2c6a81702641e1935ba41dd93f3dfc04e0d674d3
Henry Goddard <godd9170@mylaurier.ca>
2016-09-12 21:13:03 -0400

    requiring salesforce-bulk for #14

---
+++ utils/salesforce-bulk/handler.py
@@ -0,0 +1,9 @@
+
+
+def get(event, context):
+    return { "message": "Go Serverless v1.0! Your function executed successfully!", "event": event }
+
+
+def put(event, context):
+	return { "message": "Go Serverless v1.0! Your function executed successfully!", "event": event }
+
