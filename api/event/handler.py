8bbfde4680137937ae6f246e14d64563753e49a4
Henry Goddard <godd9170@mylaurier.ca>
2016-09-13 21:39:55 -0400

    event endpoint semi functional for #12

--- api/event/handler.py
+++ api/event/handler.py
@@ -1,2 +1,77 @@
-def hello(event, context):
-    return { "message": "Go Serverless v1.0! Your function executed successfully!", "event": event }
+import hashlib
+from tools import Microservice, Credentials
+functions = Microservice() # a global instantiation of the boto lambda abstraction
+
+def generate_hash(identifier_sf_id, logged_at):
+	join = str(identifier_sf_id) + str(logged_at)
+	hash = hashlib.md5(join)
+	return hash.hexdigest()
+
+def event(event, context):
+	body = event.get('body')
+	# Decrypt Auth
+	try:
+		# Get the Salesforce Credentials
+		credentials = Credentials(body.get('client_id'))
+	except Exception, e:
+		return {'Error' : 'Unable to Retrieve Authenticate With Salesforce. Have your credentials change? If so please notify Saasli'}
+
+	# Get Triggering Object Id
+	try:
+		credentials.__dict__.update({
+			'sf_object_id' : body['sf_object_id'], #hardcoded to get the account
+			'sf_field_id' : body['sf_field_id'],
+			'sf_field_value' : body['sf_field_value'],
+			'sf_select_fields' : ['Id'] # only interested in the Id
+		})
+		record = functions.request('salesforce-rest-dev-get', credentials.__dict__)
+	except KeyError, e:
+		return {'Error' : 'Missing Parameter: %s' % e}
+
+	# Handler No Triggering Object Id
+	if record.get('Id') is None: #No triggering object found
+		#What do we do here?
+		return { "Error" : "No Such Triggering Object Found: %s" %  body['sf_field_value']}
+	
+	# Performing a 'Put' will Get The Id of the User Usage History Event Type or create a new one
+	try:
+		credentials.__dict__.update({
+			'sf_object_id' : 'User_Usage_History_Event_Type__c', #hardcoded to get the account
+			'sf_field_id' : 'Name',
+			'sf_field_value' : body['event']['name'],
+			'sf_values' : {
+				'Name' : body['event']['name']  #TODO Fix #15 so we don't need to do this
+			},
+			'sf_select_fields' : ['Id'] # only interested in the Id
+		})
+		event = functions.request('salesforce-rest-dev-put', credentials.__dict__)
+	except KeyError, e:
+		return {'Error' : 'Missing Parameter: %s' % e}
+
+
+	# Build the User Usage History upsert
+	try:
+		print event
+		print event.get('Id')
+		#build the event sf_values
+		sf_values = {
+			#'Contact__c' : body['sf_field_value'], #TODO Make this bitch polymorphic
+			'User_Usage_History_Event_Type__c' : event['Id'],
+			'Event_Date_Created_UNIX__c' : body['event']['logged_at'],
+			'Saasli_Event_Id__c' : generate_hash( record['Id'], body['event']['logged_at']) #TODO Fix #15 so we don't need to do this
+		}
+
+		credentials.__dict__.update({
+			'sf_object_id' : 'User_Usage_History__c', #hardcoded to put the User_Usage_History__c
+			'sf_field_id' : 'Saasli_Event_Id__c',
+			'sf_field_value' : generate_hash( record['Id'], body['event']['logged_at']),
+			'sf_values' : sf_values
+		})
+	except KeyError, e:
+		return {'Error' : 'Missing Parameter: %s' % e}
+	
+	# Upsert the event record
+	return functions.request('salesforce-rest-dev-put', credentials.__dict__)
+
+def events(event, context):
+	return {'Message' : 'Unimplemented'}
---
+++ api/event/tools.py
@@ -0,0 +1,41 @@
+# Thank you http://stackoverflow.com/questions/36784925/how-to-get-return-response-from-aws-lambda-function
+import boto3
+import json
+
+class Microservice(object):
+	def __init__(self):
+		self.client = boto3.client('lambda')
+
+	# Given the name of a lambda function (string) and 
+	# a dict containing they {key : value} parameters 
+	# returns the response of the lambda function
+	def request(self, function, payload):
+		return json.loads( self.client.invoke(
+			FunctionName=function,
+			InvocationType='RequestResponse',
+			Payload=json.dumps(payload).encode()
+		)['Payload'].read())
+
+
+#This class expects that stored in the 'credentials' attribute of the client 
+#is an encrypted base64 stringified piece of json.
+class Credentials(object):
+	def __init__(self, id):
+		try:
+			functions = Microservice()
+			# Get the encrypted data for the client
+			dynamoPayload = {
+				'id' : id,
+				'key' : 'id',
+				'tablename' : 'clients'
+			}
+			encryptedCredentials = functions.request('dynamodb-dev-getitem', dynamoPayload)
+			# Decrypt the client credentials
+			kmsPayload = { 
+				'cipher' : encryptedCredentials.get('credentials').get('S')
+			}
+			# Assign all the encrypted fields to class attributes
+			for key, value in json.loads(functions.request('kms-dev-decrypt', kmsPayload)).iteritems():
+				setattr(self, key, value)
+		except:
+			print 'No Such Client'
