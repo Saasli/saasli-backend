9aec5ac8122960640e524a26a33cf7a3266a1f83
Henry Goddard <godd9170@mylaurier.ca>
2016-09-23 15:46:43 -0400

    agnostic account finishes fixes #31

--- api/account/handler.py
+++ api/account/handler.py
@@ -1,25 +1,27 @@
+import copy
 from tools import Microservice, Credentials
-functions = Microservice() # a global instantiation of the boto lambda abstraction
-
 
 #Expecting a payload as defined here: https://saasli.github.io/docs/#account
 def account(event, context):
+	functions = Microservice(context.function_name)
 	body = event.get('body')
 	try:
 		# Get the Salesforce Credentials & add to query payload
-		credentials = Credentials(body.get('client_id'))
+		credentials = Credentials(body.get('client_id'), functions)
 
 		if not hasattr(credentials, 'username'):
 			return {'Error' : 'Unable to find client id'}
 
+		values = copy.deepcopy(body['account'])
+		values.pop('sf_field_value')
 		credentials.__dict__.update({
 			'sf_object_id' : 'Account', #hardcoded by virtue of endpoint being Account
 			'sf_field_id' : body['sf_field_id'],
-			'sf_field_value' : body['sf_field_value'],
-			'sf_values' : body['account']
+			'sf_field_value' : body['account']['sf_field_value'],
+			'sf_values' : values
 		})
 		# Get the first record
-		response = functions.request('salesforce-rest-dev-put', credentials.__dict__)
+		response = functions.request('salesforce-rest', 'put', credentials.__dict__)
 	except KeyError, e:
 		response = {'Error' : 'Invalid Parameters %s' % e}
 	return response
