ab40f19b5f074c131a6812ab21137cced3bb4cb1
Henry Goddard <godd9170@mylaurier.ca>
2016-09-29 11:22:25 -0400

    lose the useless account files

--- api/account/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp -R ../utils . #make a copy of the global utils directory at the service level
-source ./account_venv/bin/activate #enter the venv
-pip install -r ./requirements.txt -t ./lib #get all the uninstalled python libs in the /lib folder
-sls deploy #send this puppy up to AWS
-rm -rf ./utils #hide the body
