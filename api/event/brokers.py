03cc007291feaf294d638f1ea6c7e0457deb97c2
Henry Goddard <godd9170@mylaurier.ca>
2016-11-02 19:07:01 -0400

    finishing up the events enpoint algorithm for #12

--- api/event/brokers.py
+++ api/event/brokers.py
@@ -112,21 +112,34 @@ class EventsRequest(Request):
 		except KeyError, e:
 			raise MissingParameterError({'error' : 'No Events Array Specified'})
 
-		# pick out unique uuhet names
+
+		####
+		# 1) User Usage History Event Types (UUHET)
+		#
+		# Get and create as necessary, the SFIDs of the UUHET
+		# records that will be needed to create the events
+		####
+
+		# a) pick out unique uuhet names
+		# b) create list of sf_field_values as well
 		try:
 			eventnames = []
+			fieldvalues = []
 			i = 0
 			for event in self.eventsarray:
 				if event['event']['event_name'] not in eventnames:
 					logger.info('Event: {}'.format(event))
 					eventnames.append(event['event']['event_name'])
-					i += 1
+				if event['sf_field_value'] not in fieldvalues:
+					logger.info('Field Value: {}'.format(event))
+					fieldvalues.append(event['sf_field_value'])
+				i += 1
 		except KeyError, e:
 			logger.error('Error: {}'.format(e))
 			raise MissingParameterError({'error' : "Event {} Malformed in Event Array".format(str(i))})
 		logger.info('Unique Events: {}'.format(eventnames))
 
-		# get the uuhet id's for the unique event names
+		# c) get the uuhet id's for the unique event names from SFDC
 		try:
 			uuhet_conditions = [{
 					'a' : 'Name',
@@ -139,37 +152,103 @@ class EventsRequest(Request):
 			uuhet_id_results = self.functions.request('salesforce-bulk', 'get', uuhet_payload)["results"]
 			logger.info('Unique Events Payload Results: {}'.format(uuhet_id_results))
 		except Exception, e:
-			raise Exception({ "error" : "Couldn't Get the UUHET SFIDs"})
+			raise Exception({ "error" : "Couldn't Get the UUHET SFIDs: {}".format(e)})
 
-		# confirm that all the required uuhet's exist
+		# d) confirm that all the required uuhet's exist
 		try:
-			uuhet_mappings = {} #create an existing name:id uuhet mapping
+			self.uuhet_mappings = {} #create an existing name:id uuhet mapping
 			for uuhet_record in uuhet_id_results:
 				if (uuhet_record['Name'] in eventnames):
-					uuhet_mappings.update({uuhet_record['Name'] : uuhet_record['Id']})
+					self.uuhet_mappings.update({uuhet_record['Name'] : uuhet_record['Id']})
 					eventnames.remove(uuhet_record['Name']) #remove the matched event name from the list
 		except Exception, e:
 			raise Exception({"error" : "Error Mapping User Usage History Event Type Names: {}".format(e)})
 
-		# create the leftover uuhet's
-		try:
-			new_uuhet_records = []
-			for uuhet_name in eventnames:
-				new_uuhet_records.append({'Name' : uuhet_name})
-			new_uuhet_payload = { 'sf_object_id' : 'User_Usage_History_Event_Type__c', 'sf_records' : new_uuhet_records }
-			new_uuhet_payload.update(self.credentials.__dict__)
-			logger.info('Create New Events Payload: {}'.format(new_uuhet_payload))
-			new_uuhet_id_results = self.functions.request('salesforce-bulk', 'create', new_uuhet_payload)["results"]
-			logger.info('Create New Events Payload Results: {}'.format(new_uuhet_id_results))
-		except Exception, e:
-			raise Exception({ "error" : "Error Creating the new Event Types: {} Error: {}".format(eventnames, e)})
+		if len(eventnames) > 0: #If there are some unmatched event names, we need to make them in SFDC
+			logger.info('Creating New Events')
+		# e) create the leftover uuhet's
+			try:
+				new_uuhet_records = []
+				for uuhet_name in eventnames:
+					new_uuhet_records.append({'Name' : uuhet_name})
+				new_uuhet_payload = { 'sf_object_id' : 'User_Usage_History_Event_Type__c', 'sf_records' : new_uuhet_records }
+				new_uuhet_payload.update(self.credentials.__dict__)
+				logger.info('Create New Events Payload: {}'.format(new_uuhet_payload))
+				new_uuhet_id_results = self.functions.request('salesforce-bulk', 'create', new_uuhet_payload)["results"]
+				logger.info('Create New Events Payload Results: {}'.format(new_uuhet_id_results))
+			except Exception, e:
+				raise Exception({ "error" : "Error Creating the new Event Types: {} Error: {}".format(eventnames, e)})
 
-		# add the newly created uuhet's to the uuhet_mappings
+		# f) add the newly created uuhet's to the self.uuhet_mappings
+			try:
+				# NOTE: I'm running under the assumption that the results are in the same order as the insertion request. If not, we'll have to perform yet another query.
+				for new_uuhet_record in new_uuhet_id_results:
+					self.uuhet_mappings.update({eventnames[0] : new_uuhet_record['id']}) #Creations return lowercase 'id'
+					eventnames.pop(0) #delete the 0th name for the next iteration.
+			except Exception, e:
+				raise Exception({"error" : "Error Mapping New User Usage History Event Type Names: {}".format(e)})
+		else:
+			logger.info('All Events Exist, No New Events Required.')
+
+		####
+		# 2) Triggering Objects
+		#
+		# Get the salesforce records that are to be associated to
+		# each of the events
+		####
+
+		# a) get the triggering records to be associated from SFDC (using list from 1b))
 		try:
-			# NOTE: I'm running under the assumption that the results are in the same order as the insertion request. If not, we'll have to perform yet another query.
-			for new_uuhet_record in new_uuhet_id_results:
-				uuhet_mappings.update({eventnames[0] : new_uuhet_record['id']}) #Creations return lowercase 'id'
-				eventnames.pop(0) #delete the 0th name for the next iteration.
+			triggering_records_conditions = [{
+					'a' : self.triggeringrecordfield,
+					'op' : 'IN',
+					'b' : '({})'.format(stringify(fieldvalues))
+			}]
+			triggering_records_payload = { 'sf_object_id' : self.triggeringrecordobjecttype, 'sf_select_fields' : [self.triggeringrecordfield, 'Id'], 'sf_conditions' : triggering_records_conditions }
+			triggering_records_payload.update(self.credentials.__dict__)
+			logger.info('Triggering Record Payload: {}'.format(triggering_records_payload))
+			triggering_records_id_results = self.functions.request('salesforce-bulk', 'get', triggering_records_payload)["results"]
+			logger.info('Triggering Record Payload Results: {}'.format(triggering_records_id_results))
 		except Exception, e:
-			raise Exception({"error" : "Error Mapping New User Usage History Event Type Names: {}".format(e)})
+			raise Exception({ "error" : "Couldn't Get the triggering records"})
+
+		# b) confirm that all the triggering records do exist
+		try:
+			self.triggering_records_mappings = {} #create a mapping of sf_field_value:Id for each event
+			fieldvalues_lower = map(unicode.lower,fieldvalues) #clone a lowercase version for matching purposes
+			for triggering_record in triggering_records_id_results:
+				if (triggering_record[self.triggeringrecordfield].lower() in fieldvalues_lower): #It is possible for SFDC to return matchs with different cases. We'll compare case insensitive
+					index = fieldvalues_lower.index(triggering_record[self.triggeringrecordfield].lower()) #get the index in the lowercase list of the matching fieldvalue
+					self.triggering_records_mappings.update({fieldvalues[index] : triggering_record['Id']})
+					fieldvalues.pop(index) #get rid of the matched element from the fieldvalues
+					fieldvalues_lower.pop(index) #also apply to it's lowercase clone
+			print self.triggering_records_mappings
+		except Exception, e:
+			raise Exception({"error" : "Error Mapping User Usage History Event Type Names: {}".format(e)})
+
+		#TODO: Offer the ability to create new associating records should they not exist here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- api/event/handler.py
+++ api/event/handler.py
@@ -43,7 +43,86 @@ def event(event, context):
 
 
 def events(event, context):
-    logger.info('event endpoint hit: {}'.format(event))
-    request = EventsRequest(event, context)
-    return "Thanks"
-    #return functions.request('salesforce-bulk','create', payload)
+	logger.info('event endpoint hit: {}'.format(event))
+	request = EventsRequest(event, context)
+	sf_records = [] #instantiate the array holding all the objects for upsert
+	i = 0 # keep a count of this procedure for error clarity
+	
+	# Build the sf_records array for bulk upsertion
+	for event in request.eventsarray:
+		record = {} # instantiate the record
+		
+		try:
+			# See if the triggering record id exists
+			triggering_record_id = request.triggering_records_mappings.get(event['sf_field_value'])
+			
+			if triggering_record_id is not None: # If there isn't a record to associate it to, then we're done here.
+				# Set the triggering record id
+				try:
+					record.update({'{}__c'.format(request.triggeringrecordobjecttype) : triggering_record_id}) #Set the Polymorphic relating triggering record id
+				except Exception, e:
+					raise MissingParameterError({'error' : 'Missing or Malformed \'sf_field_value\' in event {}'.format(i)})
+
+				# Set the Saasli_Event_Id__c
+				try:
+					if event['event'].get("event_id") is None: # we'll make out own hash and use it as an id
+						event_id = generate_hash(triggering_record_id, event['event']['event_timestamp'])
+					else: # let's use the id specified
+						event_id = event['event']['event_id']
+					record.update({'Saasli_Event_Id__c' : event_id})
+				except Exception, e:
+					raise MissingParameterError({'error' : 'Malformed \'event_id\' in event {}'.format(i)})
+				
+				# Set the UUHET
+				try:
+					record.update( {'User_Usage_History_Event_Type__c' : request.uuhet_mappings[event['event']['event_name']] })
+				except Exception, e:
+					raise MissingParameterError({'error' : 'Missing or Malformed \'event_name\' in event {}'.format(i)})
+
+				# Set the Event Time
+				try:
+					record.update({'Event_Date_Created_UNIX__c' : event['event']['event_timestamp']})
+				except Exception, e:
+					raise MissingParameterError({'error' : 'Missing or Malformed \'event_timestamp\' in event {}'.format(i)})
+
+				# Set any additional values (if included)
+				try:
+					if event['event'].get('event_values') is not None:
+						record.update(event['event']['event_values'])
+				except Exception, e:
+					raise MissingParameterError({'error' : 'Malformed \'event_values\' in event {}. Ensure values are a list of Salesforce API Names to Value pairs.'.format(i)})
+
+				sf_records.append(record) #append the formatted record to the sf_records list
+		except Exception, e:
+			logger.info('Skipping Event {} for error {}'.format(i, e))
+			pass
+		i += 1 # increment the event counter
+
+	events_payload = { 
+		'sf_object_id' : 'User_Usage_History__c', # inserting into the User Usage History Object
+		'sf_records' : sf_records,
+		'external_id' : 'Saasli_Event_Id__c'
+	}
+
+	events_payload.update(request.credentials.__dict__)
+	print json.dumps(events_payload, indent=4)
+	return request.functions.request('salesforce-bulk', 'upsert', events_payload)
+
+
+    	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
