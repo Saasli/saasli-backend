b02ec2e1c31a15dc2896361d3d152893deefeda5
Henry Goddard <godd9170@mylaurier.ca>
2016-12-08 15:48:15 -0500

    postgresql poller working in lambda :tada:

--- utils/postgresql-poll/handler.py
+++ utils/postgresql-poll/handler.py
@@ -1,22 +1,34 @@
-import json
+import sys
+#sys.path.insert(0, './venv/lib/python2.7/site-packages/') #TODO: Keep an eye out for serverless recognizing venvs and packaging them automatically
+import json, psycopg2
 
 def poll(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    try:
+        print "connecting?"
+        connection = psycopg2.connect(
+            dbname="test_postgres",
+            database = "test-postgres",
+            user = "hgoddard",
+            password = "salesforce1",
+            host = "test-postgres.czqasnwpdim9.us-east-1.rds.amazonaws.com",
+            port = 5432
+        )
+        print connection
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    };
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        print cursor.fetchall()
+    except Exception, e:
+        print "whoops!: {}".format(e)
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals():
+            connection.close()
 
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "event": event
     }
-    """
+
+poll(None, None)
