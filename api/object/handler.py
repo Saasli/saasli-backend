7e5a033da42505cfa5095c890d82130df04d47c9
Henry Goddard <godd9170@mylaurier.ca>
2016-12-13 18:25:46 -0500

    perform the upsert for the bulk object endpoint closes #59

--- api/object/handler.py
+++ api/object/handler.py
@@ -6,12 +6,24 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def generate_hash(identifier_sf_id, logged_at):
-	join = str(identifier_sf_id) + str(logged_at)
-	hash = hashlib.md5(join)
-	return hash.hexdigest()
+    join = str(identifier_sf_id) + str(logged_at)
+    hash = hashlib.md5(join)
+    return hash.hexdigest()
 
 
 def objects(event, context):
-	logger.info('objects endpoint hit: {}'.format(event))
-	request = ObjectsRequest(event, context)
-	return({'Success' : True})
+    logger.info('objects endpoint hit: {}'.format(event))
+    request = ObjectsRequest(event, context)
+
+    if len(request.records) > 0: #if there are no records to upsert, don't bother trying
+        # Manufacture the payload destined for salesforce-bulk
+        upsert_payload = { 
+            'sf_object_id' : request.object,
+            'sf_records' : request.records,
+            'external_id' : request.externalId
+        }
+        upsert_payload.update(request.credentials.__dict__)
+        response = request.functions.request('salesforce-bulk', 'upsert', upsert_payload) # Fire the request
+        print response
+    
+    return({'Success' : True})
